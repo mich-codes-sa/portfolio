c19f9316316e6d6510d05825eb1dab4ff7fe6b6c
Mich Stacey <77338457+mich-codes-sa@users.noreply.github.com>
2023-05-10 20:21:23 +0200

    setup variables and added header

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import { ReactComponent as ViteLogo } from './assets/vite.svg';
 import { ReactComponent as TypescriptLogo } from './assets/typescript.svg';
 import { ReactComponent as ScssLogo } from './assets/scss.svg';
 import styles from './App.module.scss';
+import { Header } from './components/header/header';
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
         <div className={styles.App}>
+            <Header />
             <div>
                 <a href="https://vitejs.dev" target="_blank">
                     <ViteLogo
---
+++ src/components/header/header.tsx
@@ -0,0 +1,34 @@
+import classNames from 'classnames';
+import styles from './header.module.scss';
+
+export interface HeaderProps {
+    className?: string;
+}
+
+/**
+ * This component was created using Codux's Default new component template.
+ * To create custom component templates, see https://help.codux.com/kb/en/article/configuration-for-headers-and-templates
+ */
+export const Header = ({ className }: HeaderProps) => {
+    return (
+        <div className={classNames(styles.root, className)}>
+            <nav className={classNames(styles['nav-bar'], styles.root)}>
+                <a href="/home" className={styles['nav-links']}>
+                    mich-stacey
+                </a>
+                <a href="/projects" className={styles['nav-links']}>
+                    <span aria-disabled>_</span>hello
+                </a>
+                <a href="/about" className={styles['nav-links']}>
+                    <span aria-disabled>_</span>about-me
+                </a>
+                <a href="/projects" className={styles['nav-links']}>
+                    <span aria-disabled>_</span>projects
+                </a>
+                <a href="/contact" className={classNames(styles['nav-links'], styles.last)}>
+                    <span aria-disabled>_</span>contact-me
+                </a>
+            </nav>
+        </div>
+    );
+};
